21325e84a8acac57f38d79791677a482f8413752
SINDIAYU9 <sindi.122150046@student.itera.ac.id>
2024-07-08 14:58:23 +0700

    hapus yang ga perlu

--- addData.php
+++ addData.php
@@ -63,12 +63,6 @@ if (isset($_POST['simpan'])) {
                <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                          <li class="nav-item">
-                              <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                         </li>
-                         <li class="nav-item">
-                              <a class="nav-link" href="#about">About</a>
-                         </li>
-                         <li class="nav-item">
                               <a class="nav-link" href="logout.php">Logout</a>
                          </li>
                     </ul>
