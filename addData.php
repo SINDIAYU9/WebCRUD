0977689686e4209fdcb5ac1f2ec212de4e082a3f
SINDIAYU9 <sindi.122150046@student.itera.ac.id>
2024-07-08 14:54:11 +0700

    rename

--- addData.php
+++ addData.php
@@ -162,9 +162,9 @@ if (isset($_POST['simpan'])) {
      <!-- Footer -->
      <div class="container-fluid">
           <div class="row bg-dark text-white text-center">
-               <div class="col my-2" id="about">
+               <div class="col my-2" id="politeknik gajah tunggal">
                     <br><br><br>
-                    <h4 class="fw-bold text-uppercase">About</h4>
+                    <h4 class="fw-bold text-uppercase">POLITEKNIK GAJAH TUNGGAL</h4>
 
                     <p>
                         SINDI WILSON HAIQAL
